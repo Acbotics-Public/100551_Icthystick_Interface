f9d127d094562e51e33e91d1526e5e40215b285f
Sam Fladung <sfladung@acbotics.com>
2024-02-19 12:46:32 -0500

    releasing to 1.0.0

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,8 +1,8 @@
 mkdir -p docs
 
 # No spaces here or bash gets unhappy
-PROJECT_NAME=<Insert name here>
-SCHEMATIC_NAME=<Insert Schematic name here>
+PROJECT_NAME=100551
+SCHEMATIC_NAME=100549
 
 librepcb-cli open-project --export-schematics docs/${SCHEMATIC_NAME}.pdf --export-schematics docs/${SCHEMATIC_NAME}.svg --export-schematics docs/${SCHEMATIC_NAME}.png design/${PROJECT_NAME}.lpp
 git add docs/${SCHEMATIC_NAME}.pdf docs/${SCHEMATIC_NAME}.svg docs/${SCHEMATIC_NAME}.png
